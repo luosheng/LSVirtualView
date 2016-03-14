8990fff82e6fbe35dbbdd6a7e85ac68e26017580
Luo Sheng <luosheng1986@gmail.com>
2016-03-14 10:13:50 +0800

    Create diff helper.

---
+++ LSVirtualView/LSVirtualNodeDiffHelper.h
@@ -0,0 +1,13 @@
+//
+//  LSVirtualNodeDiffHelper.h
+//  LSVirtualView
+//
+//  Created by Luo Sheng on 3/14/16.
+//  Copyright © 2016 Luo Sheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LSVirtualNodeDiffHelper : NSObject
+
+@end
---
+++ LSVirtualView/LSVirtualNodeDiffHelper.m
@@ -0,0 +1,13 @@
+//
+//  LSVirtualNodeDiffHelper.m
+//  LSVirtualView
+//
+//  Created by Luo Sheng on 3/14/16.
+//  Copyright © 2016 Luo Sheng. All rights reserved.
+//
+
+#import "LSVirtualNodeDiffHelper.h"
+
+@implementation LSVirtualNodeDiffHelper
+
+@end
